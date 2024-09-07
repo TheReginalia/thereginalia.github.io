8709fda59681c15dc241fab2fd4fca8f50138ec5
TheReginalia <138192485+TheReginalia@users.noreply.github.com>
2024-09-07 17:08:47 -0600

    fvgsdfg

--- SCP/script.js
+++ SCP/script.js
@@ -6,7 +6,7 @@ function validateLogin() {
         '1': '1',
         'DanielleGonzalez': '5053318662',
         'MargaretThien': '2696716504',
-        'LysolosTom': '3019771858',
+        'TunaTerry': '2892564990',
         'EdgarGraves': '7416615711'
     };
 
